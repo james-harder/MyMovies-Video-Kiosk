aae5a9d7b74b1b37e748f24bb1c9993acf957c24
James Harder <james.harder@gmail.com>
2019-04-07 11:47:10 -0500

    Adds MyMovieDB.mdf, datasources in both projects, and DBTest.vb form in AM

---
+++ Administrator Module/DBTestForm.vb
@@ -0,0 +1,14 @@
+﻿Public Class DBTestForm
+    Private Sub TableBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs) Handles TableBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.TableBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.MyMoviesDBDataSet)
+
+    End Sub
+
+    Private Sub DBTestForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'MyMoviesDBDataSet.Table' table. You can move, or remove it, as needed.
+        Me.TableTableAdapter.Fill(Me.MyMoviesDBDataSet.Table)
+
+    End Sub
+End Class
